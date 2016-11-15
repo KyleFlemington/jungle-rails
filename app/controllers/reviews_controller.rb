34d984bc5de14585c5797196d3247dbc35944e8b
Kyle <Kyleflemington@gmail.com>
2016-11-15 23:05:01 +0000

    Finished basic requirments. TODO: Review tech specs, refactor, CSS & Styling

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,9 +3,15 @@ class ReviewsController < ApplicationController
 	def create
 		@product = Product.find(params[:product_id])
 		@review = @product.reviews.new(review_params)
-		byebug
 		@review.user = current_user
 		@review.save
+		redirect_to @product
+	end
+
+	def delete
+		@product = Product.find(params[:product_id])
+		@review = @product.reviews.new(review_params)
+		@review.destroy
 		redirect_to @product
 	end
 
